69449ffaa0f6d67dfca8013fed17d71684c7e764
mikeygbd <mikey.soares@gmail.com>
2019-03-26 13:54:28 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,12 +17,14 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
+    # @student = Student.find(params[:id])
   end
 
   def index
     @students = Student.all
+
   end
+
 
   def student_params
     params.require(:student).permit(:name, :birthday, :hometown)
